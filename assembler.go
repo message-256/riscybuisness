eea15b2bdd96cb877454cca53c80bc7a350d8b95
not specified <johnjojojojojo@proton.me>
2026-07-10 23:58:12 -0400

    accidentally left some debug in vm.go
    gave it an assembler

--- assembler.go
+++ assembler.go
@@ -4,14 +4,15 @@ import (
 	"slices"
 	//"bufio"
 	"fmt"
-	"strings"
-	"strconv"
 	"os"
+	"strconv"
+	"strings"
 )
-func paddparseh(in int64,bit int) string {
-	parsed := strconv.FormatInt(in,16)
+
+func paddparseh(in int64, bit int) string {
+	parsed := strconv.FormatInt(in, 16)
 	output := parsed
-	for i := 0; i<bit-len(parsed); i++ {
+	for i := 0; i < bit-len(parsed); i++ {
 		output = "0" + output
 	}
 	return output
@@ -36,66 +37,64 @@ func main() {
 		"move":  13,
 		"movl":  14,
 		"movg":  15,
-		"dump": 16,
+		"dump":  16,
 	}
 	var registers = map[string]int{
-		"cmpr":0,
-		"outputr":1,
-		"insr":2,
-		"insp":3,
-		"addr1":4,
-		"addr2":5,
-		"intr":6,
-		"r1":7,
-		"r2":8,
-		"r3":9,
-		"r4":10,
-		"r5":11,
-		"r6":12,
-		"r7":13,
-		"r8":14,
-		"r9":15,
-		"r10":16,
-
-
+		"cmpr":    0,
+		"outputr": 1,
+		"insr":    2,
+		"insp":    3,
+		"addr1":   4,
+		"addr2":   5,
+		"intr":    6,
+		"r1":      7,
+		"r2":      8,
+		"r3":      9,
+		"r4":      10,
+		"r5":      11,
+		"r6":      12,
+		"r7":      13,
+		"r8":      14,
+		"r9":      15,
+		"r10":     16,
 	}
-	input ,err := os.ReadFile(os.Args[1])
+	input, err := os.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Fprintln(os.Stderr,"cant open file",err)
+		fmt.Fprintln(os.Stderr, "cant open file", err)
 	}
 	stringedinput := string(input)
-	stringedinput = strings.ReplaceAll(stringedinput,"\t","")
-	lines := strings.Split(stringedinput,"\n");
-	lines = slices.DeleteFunc(lines,func(s string) bool{return s == ""})
+	stringedinput = strings.ReplaceAll(stringedinput, "\t", "")
+	lines := strings.Split(stringedinput, "\n")
+	lines = slices.DeleteFunc(lines, func(s string) bool { return s == "" })
 	var output string
-	var ra,rb int
+	var ra, rb int
 	var ok bool
-	for i:= range lines {
-		splitstring := strings.Split(lines[i]," ")
+	for i := range lines {
+		splitstring := strings.Split(lines[i], " ")
 		if len(splitstring) != 2 {
-			fmt.Fprintln(os.Stderr,"no operand on line",i,"(\"",lines[i],"\")")
+			fmt.Fprintln(os.Stderr, "no operand on line", i, "(\"", lines[i], "\")")
 			continue
 		}
-		operand := instructions[splitstring[0]];
-		registersstring := strings.Split(splitstring[1],",");
-		ra,err = strconv.Atoi(registersstring[0]);
+		operand := instructions[splitstring[0]]
+		registersstring := strings.Split(splitstring[1], ",")
+		ra, err = strconv.Atoi(registersstring[0])
 		if err != nil {
-			ra,ok = registers[registersstring[0]];
+			ra, ok = registers[registersstring[0]]
 			if !ok {
-				fmt.Fprintln(os.Stderr,"error ",registersstring[0],"not a known register nor is it a number","(",err,")")
+				fmt.Fprintln(os.Stderr, "error ", registersstring[0], "not a known register nor is it a number", "(", err, ")")
 			}
 
 		}
-		rb,err = strconv.Atoi(registersstring[1])
+		rb, err = strconv.Atoi(registersstring[1])
 		if err != nil {
-			rb,ok = registers[registersstring[1]]
+			rb, ok = registers[registersstring[1]]
 			if !ok {
-				fmt.Fprintln(os.Stderr,"error ",registersstring[1],"not a known register nor is it a number","(",err,")")
+				fmt.Fprintln(os.Stderr, "error ", registersstring[1], "not a known register nor is it a number", "(", err, ")")
 			}
 		}
-		output+=paddparseh(int64(operand),2) + paddparseh(int64(ra),2) + paddparseh(int64(rb),2) + "\n"
+		output += paddparseh(int64(operand), 2) + paddparseh(int64(ra), 2) + paddparseh(int64(rb), 2) + "\n"
 
 	}
-	
-	fmt.Print(output);
+
+	fmt.Print(output)
 }
--- vm.go
+++ vm.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
-	"strconv"
-	"errors"
-	"strings"
 	"slices"
+	"strconv"
+	"strings"
 )
 
 var addresses []int64 = make([]int64, 0xfffF)
@@ -39,62 +39,49 @@ type instruction struct {
 
 var instructionstack [64 * 1000]instruction
 var instructions = []func(int64, int64){
-		ld,
-		mov,
-		add,
-		mul,
-		div,
-		sub,
-		not,
-		and,
-		or,
-		xor,
-		cmp,
-		movne,
-		move,
-		movl,
-		movg,
-		dump,
+	ld,
+	mov,
+	add,
+	mul,
+	div,
+	sub,
+	not,
+	and,
+	or,
+	xor,
+	cmp,
+	movne,
+	move,
+	movl,
+	movg,
 }
-var registers = []string {
-		"cmpr",
-		"outputr",
-		"insr",
-		"insp",
-		"addr1",
-		"addr2",
-		"intr",
-		"r1",
-		"r2",
-		"r3",
-		"r4",
-		"r5",
-		"r6",
-		"r7",
-		"r8",
-		"r9",
-		"r10",
+var registers = []string{
+	"cmpr",
+	"outputr",
+	"insr",
+	"insp",
+	"addr1",
+	"addr2",
+	"intr",
+	"r1",
+	"r2",
+	"r3",
+	"r4",
+	"r5",
+	"r6",
+	"r7",
+	"r8",
+	"r9",
+	"r10",
+}
 
-
-	}
 func bint64(o bool) int64 {
 	if o {
 		return 1
 	}
 	return 0
 }
-func dump(r1,r2 int64){
-	
-	for i := cmpr; i< r10; i++ {
-		if i == addr1 || i == addr2 {
-			fmt.Println(registers[i],"=",registers[addresses[i]])
 
-		} else {
-			fmt.Println(registers[i],"=",addresses[i])
-		}
-	}
-	fmt.Println("done")
-}
 func ld(register, number int64) {
 	if register == outputr {
 		fmt.Println(number)
@@ -102,7 +89,7 @@ func ld(register, number int64) {
 	addresses[register] = number
 }
 func mov(registera, registerb int64) {
-	ld(registera,addresses[registerb])
+	ld(registera, addresses[registerb])
 }
 func add(registera, registerb int64) {
 	addresses[registera] += addresses[registerb]
@@ -154,35 +141,34 @@ func movg(registera, registerb int64) {
 		mov(registera, registerb)
 	}
 }
-func parse(line string) (instruction,error) {
+func parse(line string) (instruction, error) {
 	var given error
 	var returned instruction
 	var err error
 	if len(line) != 6 {
-		return returned,errors.New("instruction length wrong"+ line+ "(length "+ strconv.Itoa(len(line)) + ") is not 6")
+		return returned, errors.New("instruction length wrong" + line + "(length " + strconv.Itoa(len(line)) + ") is not 6")
 	}
 	returned.operand, err = strconv.ParseInt(string(line[0:2]), 16, 32)
 	if err != nil {
-		given = errors.Join(given,err)
+		given = errors.Join(given, err)
 	}
 	returned.ra, err = strconv.ParseInt(string(line[2:4]), 16, 32)
 	if err != nil {
-		given = errors.Join(given,err)
+		given = errors.Join(given, err)
 	}
 	returned.rb, err = strconv.ParseInt(string(line[4:6]), 16, 32)
 	if err != nil {
-		given = errors.Join(given,err)
+		given = errors.Join(given, err)
 	}
-	return returned,given
-	
+	return returned, given
+
 }
-func run () {
+func run() {
 	for instructionstack[addresses[insp]].operand != 0 && addresses[insp] < int64(len(instructionstack)) {
 		addresses[insr] = instructionstack[addresses[insp]].operand
 		addresses[addr1] = instructionstack[addresses[insp]].ra
 		addresses[addr2] = instructionstack[addresses[insp]].rb
 		instructions[addresses[insr]-1](addresses[addr1], addresses[addr2])
-		dump(0,0)
 
 		addresses[insp]++
 	}
@@ -190,31 +176,31 @@ func run () {
 }
 func main() {
 	if len(os.Args) > 1 {
-		bytes ,err := os.ReadFile(os.Args[1])
+		bytes, err := os.ReadFile(os.Args[1])
 		if err != nil {
-			fmt.Fprintln(os.Stderr,err);
-			os.Exit(-1);
-		}
-		lines := strings.Split(string(bytes),"\n")
-		if len(lines) > len(instructionstack) {
-			fmt.Fprintln(os.Stderr,"file larger than instruction stack")
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(-1)
 		}
-		lines = slices.DeleteFunc(lines,func(s string) bool{return s == ""})
+		lines := strings.Split(string(bytes), "\n")
+		if len(lines) > len(instructionstack) {
+			fmt.Fprintln(os.Stderr, "file larger than instruction stack")
+			os.Exit(-1)
+		}
+		lines = slices.DeleteFunc(lines, func(s string) bool { return s == "" })
 
 		var newerr error
 		for i := range lines {
-			instructionstack[i],newerr = parse(lines[i]);
-			err = errors.Join(err,newerr)
+			instructionstack[i], newerr = parse(lines[i])
+			err = errors.Join(err, newerr)
 		}
 		if err != nil {
-			fmt.Fprintln(os.Stderr,err);
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(-1)
 		}
 		run()
 		os.Exit(0)
 	}
-	
+
 	var current instruction
 	var humaninstruction string
 	var err error
@@ -223,12 +209,12 @@ func main() {
 		fmt.Print(addresses[insp], ":")
 		humaninstruction, err = stdin.ReadString('\n')
 		if err != nil {
-			fmt.Println(err);
+			fmt.Println(err)
 			continue
 		}
-		humaninstruction = strings.ReplaceAll(humaninstruction," ","")
+		humaninstruction = strings.ReplaceAll(humaninstruction, " ", "")
 		humaninstruction = humaninstruction[:len(humaninstruction)-1]
-		current,err = parse(humaninstruction)
+		current, err = parse(humaninstruction)
 		if err != nil {
 			fmt.Println(err)
 			continue
